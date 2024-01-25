8b755bd9ac4980159d0ed083323d53bc8b68c6af
kxxdhdn <zayintopology@gmail.com>
2024-01-25 13:03:37 +0100

    CRUD Message

--- src/main/java/com/sip/gestibanque/controllers/MessageController.java
+++ src/main/java/com/sip/gestibanque/controllers/MessageController.java
@@ -2,13 +2,12 @@ package com.sip.gestibanque.controllers;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import com.sip.gestibanque.entities.Message;
 import com.sip.gestibanque.repositories.MessageRepository;
@@ -28,29 +27,53 @@ public class MessageController {
 		Message message =  new Message();
 		message.setSujet("Cursus Java");	
 		message.setDescription("Bonjour Mr Amine, plus de détails sur le cursus");
-		message.setDateCreation(LocalDate.now());
-		message.setEmail("anne@gmail.com");
+		message.setDateMessage(LocalDate.now());
+		message.setEmailSender("anne@gmail.com");
 		
-	
 		Message savedMessage = messageRepository.save(message);  // save : insert
+		
 		return savedMessage.toString();
 	}
 	
 	@RequestMapping("/list")
-	@ResponseBody
-	public String getAllMessages()
+//	@ResponseBody
+	public String getAllMessages(Model model)
 	{
 		List<Message> messages = (List<Message>) messageRepository.findAll();  // select *
-		return messages.toString();
+		model.addAttribute("messages", messages);
+		
+//		return messages.toString();
+		return "message/listMessage";
 	}
 	
 	@RequestMapping("/delete/{id}")
-	@ResponseBody
+//	@ResponseBody
 	public String deleteMessage(@PathVariable("id") int id)
 	{
 		messageRepository.deleteById(id);  // delete
-		List<Message> messages = (List<Message>) messageRepository.findAll(); 
-		return messages.toString();
+		
+		return "redirect:../list";
+	}
+	
+	@GetMapping("/update/{id}")
+	public String getFormUpdateMessage(@PathVariable("id") int id, Model model)
+	{
+		Optional<Message> opMessage = messageRepository.findById(id);
+		Message message = opMessage.get(); //message qui est remplis depuis la base
+		
+		model.addAttribute("message", message);
+		
+//		return messages.toString();
+		return "message/updateMessage";
+	}
+	
+	@PostMapping("/update")
+	//@ResponseBody
+	public String updateMessage(Message message)
+	{
+		messageRepository.save(message);  // save : insert
+		
+		return "redirect:list";
 	}
 
 }
--- src/main/java/com/sip/gestibanque/entities/Message.java
+++ src/main/java/com/sip/gestibanque/entities/Message.java
@@ -12,68 +12,68 @@ public class Message {
 	@Id // clé primaire et unique
 	@GeneratedValue(strategy = GenerationType.AUTO) // Hibernate qui va générer l'id automatiquement avant l'insertion
 	private int id;
-	
-	
 	private String sujet;
 	private String description;
-	private LocalDate dateCreation; 
-	private String email;
+	private LocalDate dateMessage; 
+	private String emailSender;
+	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getSujet() {
 		return sujet;
 	}
+
 	public void setSujet(String sujet) {
 		this.sujet = sujet;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public LocalDate getDateCreation() {
-		return dateCreation;
+
+	public LocalDate getDateMessage() {
+		return dateMessage;
 	}
-	public void setDateCreation(LocalDate dateCreation) {
-		this.dateCreation = dateCreation;
+
+	public void setDateMessage(LocalDate dateMessage) {
+		this.dateMessage = dateMessage;
 	}
-	public String getEmail() {
-		return email;
+
+	public String getEmailSender() {
+		return emailSender;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+
+	public void setEmailSender(String emailSender) {
+		this.emailSender = emailSender;
 	}
-	/*
-	public Message(int id, String sujet, String description, LocalDate dateCreation, String email) {
+	
+	@Override
+	public String toString() {
+		return "Message [id=" + id + ", sujet=" + sujet + ", description=" + description + ", dateMessage="
+				+ dateMessage + ", emailSender=" + emailSender + "]";
+	}
+
+	public Message(int id, String sujet, String description, LocalDate dateMessage, String emailSender) {
 		super();
 		this.id = id;
 		this.sujet = sujet;
 		this.description = description;
-		this.dateCreation = dateCreation;
-		this.email = email;
-	}*/
-	
-	public Message(String sujet, String description, LocalDate dateCreation, String email) {
-	
-		this.sujet = sujet;
-		this.description = description;
-		this.dateCreation = dateCreation;
-		this.email = email;
+		this.dateMessage = dateMessage;
+		this.emailSender = emailSender;
 	}
-	
+
 	public Message() {
 	
 	}
-	@Override
-	public String toString() {
-		return "Message [id=" + id + ", sujet=" + sujet + ", description=" + description + ", dateCreation="
-				+ dateCreation + ", email=" + email + "]";
-	}
 	
-
 }

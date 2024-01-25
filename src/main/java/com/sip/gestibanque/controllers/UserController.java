e78749e280fa8f02ae1f32b7a3a84e57cec45836
fatima benarab <fatimabenarab9@gmail.com>
2024-01-25 12:06:53 +0100

    Crud user

---
+++ src/main/java/com/sip/gestibanque/controllers/UserController.java
@@ -0,0 +1,83 @@
+package com.sip.gestibanque.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import com.sip.gestibanque.entities.User;
+import com.sip.gestibanque.repositories.UserRepository;
+
+
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+	
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	
+	@GetMapping("/save")
+	public String getFormAddUser(Model model)
+	{
+		
+		User user = new User();
+		model.addAttribute("user", user);
+		return "user/adduser";
+	}
+	
+	@PostMapping("/save")
+	//@ResponseBody
+	public String saveUser(User user)
+	{
+		userRepository.save(user);  
+		
+		return "redirect:list";
+	}
+	
+	@RequestMapping("/list")
+	//@ResponseBody
+	public String getUsers(Model model)
+	{
+		List<User> users = (List<User>) userRepository.findAll();  
+		model.addAttribute("users", users);
+		
+		return "user/listuser";
+	}
+	
+	@GetMapping("/delete/{id}")
+	//@ResponseBody
+	public String deleteUser(@PathVariable("id") int id)
+	{
+		userRepository.deleteById(id);
+		return "redirect:../list";
+	}
+	
+	@GetMapping("/update/{id}")
+	public String getFormUpdateUser(@PathVariable("id") int id, Model model)
+	{
+		Optional<User> opUser = userRepository.findById(id);
+		User user = opUser.get(); 
+		
+		model.addAttribute("user", user);
+		
+		return "user/updateUser";
+	}
+	
+	@PostMapping("/update")
+	//@ResponseBody
+	public String updateBanque(User user)
+	{
+		userRepository.save(user); 
+		return "redirect:list";
+	}
+	
+
+}

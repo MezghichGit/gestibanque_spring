78a801c34d4d391374a83ffe4b0bd2facc158d75
gilert <crankyfox@protonmail.com>
2024-01-25 14:38:29 +0100

    list comptes and save compte

--- src/main/java/com/sip/gestibanque/controllers/CBController.java
+++ src/main/java/com/sip/gestibanque/controllers/CBController.java
@@ -1,12 +1,16 @@
 package com.sip.gestibanque.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sip.gestibanque.entities.Banque;
 import com.sip.gestibanque.entities.CompteBancaire;
 import com.sip.gestibanque.repositories.CompteRepository;
 import com.sip.gestibanque.repositories.MessageRepository;
@@ -29,8 +33,13 @@ public class CBController {
 	public String saveCompte(CompteBancaire cb) {
 		compteRepository.save(cb);
 		return "redirect:list";
-		
-		
+	}
+	
+	@GetMapping("/list")
+	public String getAllComptes(Model model) {
+		List<CompteBancaire> comptes = (List<CompteBancaire>) compteRepository.findAll();  // select *
+		model.addAttribute("comptes", comptes);
+		return "compte/listeCompte";
 	}
 	
 	

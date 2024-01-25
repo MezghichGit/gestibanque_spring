0d1e7048ef49c633e1c10388f2aa2e2686d282ae
gilert <crankyfox@protonmail.com>
2024-01-25 14:20:05 +0100

    add listeCompte and improve CompteRepository and CBController

--- src/main/java/com/sip/gestibanque/controllers/CBController.java
+++ src/main/java/com/sip/gestibanque/controllers/CBController.java
@@ -3,6 +3,7 @@ package com.sip.gestibanque.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,6 +23,14 @@ public class CBController {
 		CompteBancaire cb = new CompteBancaire();
 		model.addAttribute("cb", cb);
 		return "compte/compteForm";
+	}
+	
+	@PostMapping("/save")
+	public String saveCompte(CompteBancaire cb) {
+		compteRepository.save(cb);
+		return "redirect:list";
+		
+		
 	}
 	
 	
--- src/main/java/com/sip/gestibanque/repositories/CompteRepository.java
+++ src/main/java/com/sip/gestibanque/repositories/CompteRepository.java
@@ -3,7 +3,8 @@ package com.sip.gestibanque.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import com.sip.gestibanque.entities.Banque;
+import com.sip.gestibanque.entities.CompteBancaire;
 
-public interface CompteRepository extends CrudRepository<Banque, Integer> {
+public interface CompteRepository extends CrudRepository<CompteBancaire, Integer> {
 
 }
